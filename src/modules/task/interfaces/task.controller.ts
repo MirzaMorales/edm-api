96d42d15b891adf140a58f95e9151b90179c6c21
MirzaMorales <natzllyuni@gmail.com>
2026-02-09 19:58:35 -0600

    controlador task

--- src/modules/task/interfaces/task.controller.ts
+++ src/modules/task/interfaces/task.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
 import { TaskService } from "./task.service";
 
 @Controller("/api/task")
@@ -9,5 +9,25 @@ export class TaskController {
     @Get("/get-tasks")
     public getTasks(): string{
         return this.taskSvc.getTasks();
+    }
+
+    @Get(":id")
+    public getTaskById(@Param("id") id: string): any{   
+        return this.taskSvc.getTaskById(parseInt(id));
+    }
+
+    @Post("/insert-task")
+    public insertTask(task: any): any{
+        return this.taskSvc.insertTask(task);
+    }
+
+    @Put(":id")
+    public updateTask(id: number, task: any): any{      
+        return this.taskSvc.updateTask(id, task);
+    }
+
+    @Delete(":id")
+    public deleteTask(id: number): string{
+        return this.taskSvc.deleteTask(id);
     }
 }
--- src/modules/task/interfaces/task.service.ts
+++ src/modules/task/interfaces/task.service.ts
@@ -6,4 +6,22 @@ export class TaskService {
     public getTasks(): string{
         return "Lista de tareas";
     }
+
+    public getTaskById(id: number): string{
+        return `Tarea con id: ${id}`;
+    }
+
+    public insertTask(task: any): any{
+        return task
+    }
+
+    public updateTask(id: number, task: any): any{
+        return task;
+    }
+
+    public deleteTask(id: number): string{
+        return `Tarea con id: ${id} eliminada`;
+    }
+
+
 }
